e72c96de92c69589b4d8f0ff711557b53fc8592b
Joey Mark Llyod Chavez <joey.chavez@nokia.com>
2017-11-14 01:37:01 +0800

    [FIX] Make Dynamic range number.

--- sumfromlist.py
+++ sumfromlist.py
@@ -1,2 +1,2 @@
-def sum_from_list():
-  return sum([x for x in range(1,1000) if x % 3 == 0 or x % 5 == 0])
+def sum_from_list(range_num):
+  return sum([x for x in range(1,range_num) if x % 3 == 0 or x % 5 == 0])
