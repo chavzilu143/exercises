9838c3f7f8f40747e2c36ad413434e653d4b1686
Joey Mark Llyod Chavez <33633175+chavzilu143@users.noreply.github.com>
2017-11-17 10:41:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import sum_from_list from sumfromlist
+import get_largest_prime from primefactors
+import sum_fibonacci_with_even from fibonacci
 
 #For Question Number 1.
 print(sum_from_list(1000))
